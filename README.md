70dfd901922b81d063b9e265e7d47439c1737afe
anton-github <shockfront00@gmail.com>
2019-10-28 14:54:39 +0500

    add desktop first & improve docs

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # Device Observer
 
+When you style you components in CSS using media queries it's quite often when for a particular component CSS 
+is just not powerful enough to fulfill you needs.
+Then you have to use JS for that purpose so you copy your breakpoint and use something like 
+window.addEventListener('resize', ...) and rely on window.outerWidth/innerWidth.
+
+Besides in some cases window.outerWidth/innerWidth can be inconsistent in different browsers (see [here](https://delete-agency.github.io/device-observer/examples/window-width-test.html))
+so relying on them is not very safe, adding another addEventListener and "window.innerWidth > 1280" 
+just makes your code less maintainable.
+
+Device Observer allows you to configure your breakpoints once and then write media queries conditions in JS the same way you are used to in CSS
+<br>It uses window.matchMedia that always returns correct results.
+Supports both mobile and desktop-first approaches (can be configured via options).
+
 [Live Demo](https://delete-agency.github.io/device-observer/)
-
-## Motivation
-
-TODO 
 
 ## Installation
 
@@ -25,34 +34,65 @@ deviceObserver.init({
     'desktop': 1024
 });
 
-deviceObserver.subscribeOnChange(() => {
-    if(deviceObserver.is('>=', 'tablet')){
-        console.log('Tablet and above. Do dome logic')
+if(deviceObserver.is('>=', 'tablet')){
+    console.log('Tablet and above. Do dome logic')
+}
+
+deviceObserver.subscribeOnChange((newDevice, oldDevice) => {
+    if(deviceObserver.is('<', 'tablet')){
+        console.log('Small device was detected. Do dome logic')
     }
 })
 ```
 
 ## API
 
-### deviceObserver.init(devices, [debounceMs])
-
+### init(devices, options = {})
 Sets options described in Options section
 
 #### options
-
 *Required*<br>
 Type: `Object`
 
-### deviceObserver.subscribeOnChange(cb)
+##### resizeDebounce 
+Debounce time in milliseconds for handling window resize event. Default value - `50`. 
 
+##### mobileFirst
+Whether to use mobileFirst approach in current device determining. Default value - `true`
+
+### subscribeOnChange(cb)
 Subscribe on device change. Passed callback function will be called every time device is changes 
 (in terms of user devices passed as the first argument to deviceObserver.init())
 
 #### cb
-
 *Required*<br>
 Type: `Function`
 
+Callback function what has the following signature: `function cb(newDeviceName, oldDeviceName)`
+
+### isMatch(deviceName), isGt(deviceName), isGe(deviceName), isLt(deviceName), isLe(deviceName)
+Returns `Boolean`
+
+Returns whether current device matches (is greater then, is greater or even, is lower then, is lower or even) passed device name
+
+#### deviceName
+*Required*<br>
+Type: `string`
+
+### is(operator, deviceName) 
+Returns `Boolean`
+
+Just an alias for the above methods
+
+#### operator
+*Required*<br>
+Type: `string`
+
+Allowed values: '=', '>', '>=', '<', '<='
+
+#### deviceName
+*Required*<br>
+Type: `string`
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- src/device-observer.js
+++ src/device-observer.js
@@ -2,6 +2,7 @@ const DEFAULT_RESIZE_DEBOUNCE = 100;
 
 class DeviceObserver {
     constructor() {
+        this._inited = false;
         this._devices = {};
         this._ascendingDevicesArray = [];
         this._deviceName = null;
@@ -19,13 +20,18 @@ class DeviceObserver {
      *   'tablet': 768,
      *   'desktop:' 1024,
      * }
-     * @param resizeDebounce
+     * @param {Object} options
      */
-    init(devices, resizeDebounce) {
+    init(devices, options = {}) {
         this._setDevicesData(devices);
-        this._resizeDebounce = resizeDebounce || DEFAULT_RESIZE_DEBOUNCE;
+        this._resizeDebounce = 'resizeDebounce' in options ? options.resizeDebounce : DEFAULT_RESIZE_DEBOUNCE;
+        this._mobileFirst = 'mobileFirst' in options ? options.mobileFirst : true;
         this._updateDeviceData();
-        window.addEventListener('resize', this._onWindowResize.bind(this));
+
+        if (!this._inited) {
+            this._inited = true;
+            window.addEventListener('resize', this._onWindowResize.bind(this));
+        }
     }
 
     /**
@@ -42,9 +48,6 @@ class DeviceObserver {
 
         if (this._ascendingDevicesArray.length === 0) {
             throw new Error('No devices specified');
-        }
-        if (this._ascendingDevicesArray[0][1] !== 0) {
-            throw new Error('Smallest device name must have breakpoint equal 0');
         }
     }
 
@@ -71,10 +74,32 @@ class DeviceObserver {
         }
     }
 
+    _checkDevices() {
+        if (this._ascendingDevicesArray.length === 0) {
+            throw new Error('No devices specified');
+        }
+
+        if (this._mobileFirst && this._ascendingDevicesArray[0][1] !== 0) {
+            throw new Error('The smallest device must have breakpoint equal 0 in case mobile-first approach is used');
+        }
+
+        if (!this._mobileFirst && this._ascendingDevicesArray[this._ascendingDevicesArray.length - 1][1] !== Infinity) {
+            throw new Error('The greatest device name must have breakpoint equal Infinity in case desktop-first approach is used');
+        }
+    }
+
     _updateDeviceData() {
-        // reverse array because of mobile-first
-        // create the new array before reverse not to mutate original one
-        const currentDevice = [...this._ascendingDevicesArray].reverse().find(deviceData => this._isGreaterOrEqual(deviceData[1]));
+        this._checkDevices();
+
+        let currentDevice;
+        if (this._mobileFirst) {
+            // reverse array because of mobile-first
+            // create the new array before reverse not to mutate original one
+            currentDevice = [...this._ascendingDevicesArray].reverse().find(deviceData => this._isGreaterOrEqual(deviceData[1]));
+        } else {
+            currentDevice = this._ascendingDevicesArray.find(deviceData => this._isLowerOrEqual(deviceData[1]));
+        }
+
         this._deviceIndex = this._ascendingDevicesArray.indexOf(currentDevice);
         this._deviceName = currentDevice[0];
     }
@@ -91,6 +116,15 @@ class DeviceObserver {
         return matchMedia(`only screen and (min-width:${pixelsValue}px)`).matches;
     }
 
+    _isLowerOrEqual(pixelsValue) {
+        // convert Infinity to some really big number
+        // to make sure matchMedia below returns true in that case
+        if (pixelsValue === Infinity) {
+            pixelsValue = 100000;
+        }
+        return matchMedia(`only screen and (max-width:${pixelsValue}px)`).matches;
+    }
+
     _getDeviceNameIndex(deviceName) {
         return this._ascendingDevicesArray.findIndex(deviceData => deviceData[0] === deviceName);
     }
@@ -104,11 +138,8 @@ class DeviceObserver {
         };
     }
 
-    subscribeOnChange(cb, getInitialValue = false) {
+    subscribeOnChange(cb) {
         this._deviceChangeCallbacks.push(cb);
-        if (getInitialValue) {
-            cb(this._deviceName);
-        }
 
         // return unsubscribe method
         return () => {
